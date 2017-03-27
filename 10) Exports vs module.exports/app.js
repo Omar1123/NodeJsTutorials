b397da1c68396ab63bbc24dca5aa280f23365bc6
Plusti <omar1123veliz@gmail.com>
2017-03-27 15:23:58 -0600

    ADD: Exports vs module.exports

---
+++ 10) Exports vs module.exports/app.js
@@ -0,0 +1,7 @@
+//module.exports is a object
+//exports = object
+//Mutate: Change something, add new properties
+
+var greet = require('./greet.js');
+var greet2 = require('./greet2.js');
+greet2.greet();
