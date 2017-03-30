300becf89b5e95c5a1918022aebb0b18c439495a
Plusti <omar1123veliz@gmail.com>
2017-03-30 14:53:03 -0600

    ADD: Templates, routes

--- 17) Template/app.js
+++ 17) Template/app.js
@@ -3,9 +3,13 @@
 //Middleware: Code that sits between two layers of software
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 4000;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 
 app.use('/assets', express.static(__dirname + '/public'));
 
@@ -21,7 +25,20 @@ app.get('/', function(req,res) {
 });
 
 app.get('/person/:id', function(req,res) {
-    res.render('person', { ID: req.params.id });
+    res.render('person', { ID: req.params.id, Qstr:
+    req.query.qstr });
+});
+
+app.post('/person', urlencodedParser ,function(req,res) {
+    res.render('Thank you!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function(req, res) {
+    res.send('Thank you for the JSON data!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
 });
 
 app.get('/api', function(req,res) {
