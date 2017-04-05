ad2f8b6a9b8a3927efbceab98f9ca02eb416bf9f
Plusti <omar1123veliz@gmail.com>
2017-04-05 16:22:56 -0600

    ADD: New node js file

---
+++ 21) Asynchronous code/app.js
@@ -0,0 +1,4 @@
+//Asynchronous: More that one process running simulateneously
+//Synchronous: One proccess executing at time
+//Callbacks: A function passed to some other function which we assume will be invoked at some point
+//Non-blocking: Doing other things without stopping your programming from running
